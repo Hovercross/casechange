b9c828733c4dc64b0170ec9cd19bebf63bda620f
Adam Peacock <adam@thepeacock.net>
2021-11-05 16:03:53 -0400

    Add CLI interface

---
+++ casechange/cli.py
@@ -0,0 +1,11 @@
+"""Command line interface to case changer"""
+
+import sys
+
+from .casechange import lowercase_stream, uppercase_stream
+
+def to_lower():
+    lowercase_stream(sys.stdin, sys.stdout)
+
+def to_upper():
+    uppercase_stream(sys.stdin, sys.stdout)
