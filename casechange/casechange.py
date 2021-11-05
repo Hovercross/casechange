aee22036348360dd252a14dd5b2dc596252640eb
Adam Peacock <adam@thepeacock.net>
2021-11-05 16:18:17 -0400

    Run black formatters

--- casechange/casechange.py
+++ casechange/casechange.py
@@ -2,9 +2,11 @@
 
 from typing import TextIO
 
+
 def lowercase_stream(in_stream: TextIO, out_stream: TextIO):
     for data in in_stream:
         out_stream.write(data.lower())
+
 
 def uppercase_stream(in_stream: TextIO, out_stream: TextIO):
     for data in in_stream:
--- casechange/cli.py
+++ casechange/cli.py
@@ -4,8 +4,10 @@ import sys
 
 from .casechange import lowercase_stream, uppercase_stream
 
+
 def to_lower():
     lowercase_stream(sys.stdin, sys.stdout)
+
 
 def to_upper():
     uppercase_stream(sys.stdin, sys.stdout)
